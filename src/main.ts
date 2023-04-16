38be2117c51a99083b91d3838a514ceac28d80f8
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-04-16 11:44:35 -0300

    feat:connect with WhatsApp
    Create a bot for WhatsApp using venom-bot

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log("Ola")
+import {Whatsapp , create} from "venom-bot";
+
+
+create('sales').then((client) => start(client));
+
+ function  start(client:Whatsapp) {
+     client.onMessage((message) => {
+            if(message.body == 'Oi' || message.body ||'ola'){
+                client.sendText(message.from, 'Olá tudo bem com vc?')
+                    .then(result => console.log(result))
+                    .catch(err => console.error(err));
+            }
+    })
+}  
