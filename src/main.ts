8c9d24381a063134339d3230ca6630c0c43dde27
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-04-20 23:16:11 -0300

    Refactor: error handling improvements

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,19 @@ function start(client: Whatsapp) {
   console.log('Bot iniciado!');
 
   client.onMessage(async (message) => {
-    const { body, from } = message;
+    const { body, from, type } = message;
     console.log(`Mensagem recebida de ${from}: ${body}`);
 
+    if (type !== 'chat' || body.length < 1) {
+      return;
+    }
+
+    if (body === '!help') {
+      const response = 'Digite qualquer mensagem e eu irei respondê-la.';
+      await client.sendText(from, response);
+      console.log(`Mensagem enviada para ${from}: ${response}`);
+      return;
+    }
 
     const response = await chatGPT(body);
 
@@ -20,6 +30,8 @@ function start(client: Whatsapp) {
       console.log(`Mensagem enviada para ${from}: ${response}`);
     } catch (error:any) {
       console.error(`Erro ao enviar mensagem para ${from}: ${error.message}`);
+       await client.sendText(from, 'Desculpe, houve um erro ao enviar a mensagem.');
     }
   });
 }
+
